5ec239d1d40f9f1a7d87955be3d502bebb13586f
SerSkiner <serterion@gmail.com>
2017-11-02 14:12:54 +0200

    added populate/greate skills

---
+++ Homework1_SQL/populateDB.sql
@@ -0,0 +1,23 @@
+INSERT INTO skills (ID, NAME)
+ VALUES (1, 'Java');
+ 
+ INSERT INTO skills (ID, NAME)
+ VALUES (2, 'Python');
+ 
+ INSERT INTO skills (ID, NAME)
+ VALUES (3, 'C#');
+ 
+ INSERT INTO skills (ID, NAME)
+ VALUES (4, 'Ruby');
+ 
+  INSERT INTO skills (ID, NAME)
+ VALUES (5, 'C++');
+ 
+  INSERT INTO skills (ID, NAME)
+ VALUES (6, 'JavaScript');
+ 
+  INSERT INTO skills (ID, NAME)
+ VALUES (7, 'PHP');
+ 
+  INSERT INTO skills (ID, NAME)
+ VALUES (8, 'UI/UX');
