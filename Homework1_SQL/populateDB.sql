b7a5e6594966bca1bfea4c6d77d190107efaa46d
SerSkiner <serterion@gmail.com>
2017-11-02 16:08:51 +0200

    add user

--- Homework1_SQL/populateDB.sql
+++ Homework1_SQL/populateDB.sql
@@ -21,3 +21,27 @@ INSERT INTO skills (ID, NAME)
  
   INSERT INTO skills (ID, NAME)
  VALUES (8, 'UI/UX');
+ 
+ INSERT INTO developers 
+VALUES (1, 'Ludmila Geiko', 2, 1, 2);
+
+INSERT INTO developers 
+VALUES (2, 'Andrey Bogdanov', 7, 3, 1);
+
+INSERT INTO developers 
+VALUES (3, 'Alex Petrov', 3, 3, 8);
+
+INSERT INTO developers 
+VALUES (4, 'Misha Sergeev', 1, 8, 2);
+
+INSERT INTO developers 
+VALUES (5, 'Ivan Kutepov', 1, 1, 6);
+
+INSERT INTO developers 
+VALUES (6, 'Ivan Petrov', 1, 1, 8);
+
+INSERT INTO developers 
+VALUES (7, 'Ivan Ivanov', 2, 1, 4);
+
+
+
