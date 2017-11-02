d0e27733eaf69d6ecbbc576f9d8da7e26bd848d0
SerSkiner <serterion@gmail.com>
2017-11-02 21:30:34 +0200

    add project/companies/customers+connect

--- Homework1_SQL/initDB.sql
+++ Homework1_SQL/initDB.sql
@@ -22,3 +22,90 @@ insert into Dev_Skills (developers_id, skills_id) values
 (7,2);
 
 
+CREATE TABLE Projects
+(ID int auto_increment primary key,
+NAME varchar(50));
+
+CREATE TABLE Companies 
+(ID int auto_increment primary key,
+NAME varchar(50));
+
+CREATE TABLE Customers  
+(ID int auto_increment primary key,
+NAME varchar(50));
+
+CREATE TABLE Pro_Dev
+(Projects_ID int, Developers_ID int);
+
+alter table Pro_Dev add constraint PK_Pro_D primary key(PROJECTS_ID, DEVELOPERS_ID);
+
+insert into Pro_Dev (projects_id, developers_id) values
+(1,1),
+(1,4),
+(1,3),
+(2,3),
+(2,2),
+(3,5),
+(3,1),
+(4,3),
+(5,2),
+(5,6),
+(5,1),
+(6,2),
+(6,6);
+
+CREATE TABLE Corp
+(Companies_ID int, Developers_ID int);
+
+alter table Corp add constraint PK_Corp primary key(COMPANIES_ID, DEVELOPERS_ID);
+
+insert into Corp (companies_id, developers_id) values
+(1,1),
+(1,4),
+(1,3),
+(2,3),
+(2,2),
+(3,1),
+(3,6),
+(3,7),
+(4,2),
+(4,6),
+(4,1),
+(2,5),
+(2,4);
+
+CREATE TABLE Works
+(Companies_ID int, Projects_ID int);
+
+alter table Works add constraint PK_Works primary key(COMPANIES_ID, PROJECTS_ID);
+
+insert into Works (companies_id, projects_id) values
+(1,1),
+(1,6),
+(2,2),
+(2,4),
+(3,3),
+(3,1),
+(4,5),
+(4,6);
+
+CREATE TABLE Zakaz
+(Customers_ID int, Projects_ID int);
+
+alter table Zakaz add constraint PK_Zakaz primary key(CUSTOMERS_ID, PROJECTS_ID);
+
+insert into Zakaz (customers_id, projects_id) values
+(1,2),
+(1,6),
+(2,3),
+(2,4),
+(3,5),
+(3,6),
+(4,5),
+(4,6),
+(5,1),
+(5,5);
+
+
+
+
