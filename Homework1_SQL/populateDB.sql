593e95af96012c9a148ee7d600e875b9446c5912
SerSkiner <serterion@gmail.com>
2017-11-02 18:32:56 +0200

    added users/skills

--- Homework1_SQL/populateDB.sql
+++ Homework1_SQL/populateDB.sql
@@ -1,47 +1,18 @@
-INSERT INTO skills (ID, NAME)
- VALUES (1, 'Java');
- 
- INSERT INTO skills (ID, NAME)
- VALUES (2, 'Python');
- 
- INSERT INTO skills (ID, NAME)
- VALUES (3, 'C#');
- 
- INSERT INTO skills (ID, NAME)
- VALUES (4, 'Ruby');
- 
-  INSERT INTO skills (ID, NAME)
- VALUES (5, 'C++');
- 
-  INSERT INTO skills (ID, NAME)
- VALUES (6, 'JavaScript');
- 
-  INSERT INTO skills (ID, NAME)
- VALUES (7, 'PHP');
- 
-  INSERT INTO skills (ID, NAME)
- VALUES (8, 'UI/UX');
- 
- INSERT INTO developers 
-VALUES (1, 'Ludmila Geiko', 2, 1, 2);
-
-INSERT INTO developers 
-VALUES (2, 'Andrey Bogdanov', 7, 3, 1);
-
-INSERT INTO developers 
-VALUES (3, 'Alex Petrov', 3, 3, 8);
-
-INSERT INTO developers 
-VALUES (4, 'Misha Sergeev', 1, 8, 2);
-
-INSERT INTO developers 
-VALUES (5, 'Ivan Kutepov', 1, 1, 6);
-
-INSERT INTO developers 
-VALUES (6, 'Ivan Petrov', 1, 1, 8);
-
-INSERT INTO developers 
-VALUES (7, 'Ivan Ivanov', 2, 1, 4);
+insert into Skills (NAME) values
+('JAVA'),
+('C++'),
+('Pyton'),
+('PHP'),
+('JavaScript'),
+('UI/UX'),
+('Rubi');
 
 
-
+insert into Developers (NAME) values
+('Andrey B.'),
+('Ivan .S'),
+('Valera M.'),
+('Ivanov V.'),
+('Sasha P.'),
+('Olya S.'),
+('Masha E.');
