fc7f3a6a8edd617ff38a98c1038ab6a988aba625
SerSkiner <serterion@gmail.com>
2017-12-11 20:25:46 +0200

    remove old

--- Homework2_JDBC/src/main/java/net/home/jdbc/dao/DeveloperDAO.java
+++
@@ -1,8 +0,0 @@
-package net.home.jdbc.dao;
-
-import net.home.jdbc.dao.entiti.Developer;
-
-import java.sql.SQLException;
-
-public interface DeveloperDAO extends GenericDAO<Developer, Long> {
-}
--- Homework2_JDBC/src/main/resources/InitDB.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE developers
-(ID int auto_increment primary key,
-first_name varchar(100),
-last_name VARCHAR(100),
-salary DECIMAL NOT NULL);
-
-
-
