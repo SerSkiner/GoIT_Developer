e5060e826b34429977724b96c5b0088a6bf5e391
SerSkiner <serterion@gmail.com>
2017-12-27 12:34:30 +0200

    add sample

---
+++ Homework3_Hibernate/src/main/resources/db/populateDB.sql
@@ -0,0 +1,17 @@
+insert into skills (skill_name) values
+('Java'),
+('C++'),
+('Rubi');
+
+insert into developers (developer_name, skill_id, salary) values
+('Andrey B.', 1,  3200),
+('Sergey S.', 1,  100000),
+('anna B.', 3,  3200);
+
+insert into projects (project_name, developer_id) values
+('SecureOS', 2),
+('Accel-ppp', 1),
+('Ubunu 16.04', 3);
+
+
+
